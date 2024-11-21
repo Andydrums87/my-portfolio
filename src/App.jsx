bbf55e405b8c3c27e74614186d565e3df75ab82c
Andy Joseph <neon_4@hotmail.com>
2024-11-21 11:56:32 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import { BrowserRouter } from "react-router-dom"
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components"
+const App = () => {
+
+
+  return (
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="">
+      
+        <Navbar />
+        <div className="relative z-0">
+        <Hero />
+        <StarsCanvas />
+      </div>
+     
+        
+        </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className="relative z-0">
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </div>
+    </BrowserRouter>
+  )
+}
+
+export default App;
